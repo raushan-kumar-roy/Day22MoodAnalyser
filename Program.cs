5057e051dc1c7c6b5bc57b17b4fa6a23f99b4f6a
raushan-kumar-roy <thorbtc0101@gmail.com>
2022-11-18 18:29:05 +0530

    mood analyser

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ArthmaticProblem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            //try
+            //{
+            //    Mood mood = new Mood();
+            //    mood.Happy();
+            //}
+            //catch (MoodIsNullException m)
+            //{
+            //    Console.WriteLine(m.Message);
+            //}
+
+            try
+            {
+                Mood mood = new Mood();
+                mood.Emtpy();
+            }
+            catch (MoodIsEmptyException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+
+
+        }
+    }
+}
